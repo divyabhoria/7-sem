fb791d30d514c81137835debf41f83bb00d63fd5
Divya Bhoria <endemicspecies24@gmail.com>
2025-07-02 12:51:51 +0530

    Add files via upload
    July2

---
+++ July2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class July2{
+    
+    public static void main(String[] args){
+       // System.out.println("divya");
+       Scanner sc=new Scanner(System.in);
+ System.out.println("enter name");
+       String name=sc.nextLine();
+ System.out.println("enter roll number");
+        Double roll=sc.nextDouble();
+        System.out.println("my name "+name+"and roll number is"+roll);
+    }
+}
